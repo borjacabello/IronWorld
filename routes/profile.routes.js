d5a2c0644b70b3e7f7112a142430bcc2a1e18a90
David Lazaro <dlazaro7@gmail.com>
2022-10-22 06:35:30 +0200

    profile route and profile hbs added

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,6 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const User = require("../models/User.model");
+
 //! IMPORT MIDDLEWARES
+const {
+  isUserLoggedIn,
+  isAdmin,
+  isModeratorOrAdmin,
+} = require("../middlewares/auth.middlewares.js");
+
+// GET /profile => Renders user profile page
+router.get("/", isUserLoggedIn, async (req, res, next) => {
+  try {
+    const userOnlineDetails = await User.findById(req.session.userOnline);
+    res.render("profile/my-profile.hbs", {
+      profileDetails: userOnlineDetails,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
