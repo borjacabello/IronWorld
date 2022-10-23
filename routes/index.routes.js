91e8725eee15abe07ede4958e292df3dc07d2c0c
Borja Cabello Luna <borjacabello@outlook.es>
2022-10-24 00:46:54 +0200

    added css for publication and comments

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,45 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const User = require("../models/User.model");
 const Publication = require("../models/Publication.model");
-const Comment = require("../models/Comment.model")
-
-
+const Comment = require("../models/Comment.model");
 
 /* GET home page */
 router.get("/", async (req, res, next) => {
-
   try {
-    const publications = await Publication.find().populate("user").populate("comments")
-    //const comments = await Comment.find().populate("publication")
-    
-    res.render("index", {
-      publications
-    });
+    // Use populate({path: "id"}) to populate the users inside each comment in the publication
+    const publications = await Publication.find()
+      .populate("user")
+      .populate({ path: "comments", populate: { path: "user" } });
 
-  } catch(error) {
-    next(error)
+    res.render("index", {
+      publications,
+    });
+  } catch (error) {
+    next(error);
   }
- 
 });
 
-
 // Auth routes
-const authRoutes = require("./auth.routes")
-router.use("/auth", authRoutes)
+const authRoutes = require("./auth.routes");
+router.use("/auth", authRoutes);
 
 // Profile routes
-const profileRoutes = require("./profile.routes")
-router.use("/profile", profileRoutes)
+const profileRoutes = require("./profile.routes");
+router.use("/profile", profileRoutes);
 
 // Admin routes
-const adminRoutes = require("./admin.routes")
-router.use("/admin", adminRoutes)
+const adminRoutes = require("./admin.routes");
+router.use("/admin", adminRoutes);
 
 // User routes
-const userRoutes = require("./user.routes")
-router.use("/user", userRoutes)
-
+const userRoutes = require("./user.routes");
+router.use("/user", userRoutes);
 
 // Export
 module.exports = router;
