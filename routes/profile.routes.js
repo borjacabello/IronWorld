e180a6fdc8cd1326df8cb69ccc0ef6b3c8832545
David Lazaro <dlazaro7@gmail.com>
2022-10-24 14:42:19 +0200

    Own profile publicatios CRUD Done

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -14,72 +14,91 @@ const {
 router.get("/", isUserLoggedIn, async (req, res, next) => {
   try {
     const userOnlineDetails = await User.findById(req.session.userOnline);
-    const publicationUserOnline = await Publication.find({user: userOnlineDetails}).populate("user")
-  
+    const publicationUserOnline = await Publication.find({
+      user: userOnlineDetails,
+    }).populate("user");
+
     res.render("profile/my-profile.hbs", {
       profileDetails: userOnlineDetails,
-      publicationUserOnline: publicationUserOnline
-
+      publicationUserOnline: publicationUserOnline,
     });
-
-  
-
   } catch (error) {
     next(error);
   }
 });
 
+// *********************** OWN PUBLICATIONS ROUTES *************************************
 // GET "/profile/publications/:publicationId/details" => renders the details of each own publication
-router.get("/publications/:publicationId/details", isUserLoggedIn, async (req, res, next) => {
-  const { publicationId } = req.params;
+router.get(
+  "/publications/:publicationId/details",
+  isUserLoggedIn,
+  async (req, res, next) => {
+    const { publicationId } = req.params;
 
-  try {
-    const publicationDetails = await Publication.findById(publicationId).populate("user");
-console.log(publicationDetails)
-    res.render("publications/pending/details.hbs", {
-      publicationDetails: publicationDetails,
-    });
-  } catch (error) {
-    next(error);
+    try {
+      const publicationDetails = await Publication.findById(
+        publicationId
+      ).populate("user");
+      console.log(publicationDetails);
+      res.render("publications/pending/details.hbs", {
+        publicationDetails: publicationDetails,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-}
 );
 
 // POST "/profile/publications/:publicationId/delete" => deletes the current own publication
-router.post("/publications/:publicationId/delete", isUserLoggedIn, async (req, res, next)=> {
+router.post(
+  "/publications/:publicationId/delete",
+  isUserLoggedIn,
+  async (req, res, next) => {
+    const { publicationId } = req.params;
+    console.log(publicationId);
+    try {
+      await Publication.findByIdAndDelete(publicationId);
 
-  const {publicationId} = req.params
-  console.log(publicationId)
-  try {
-    const x = await Publication.findByIdAndDelete(publicationId)
-    console.log(x)
-    res.redirect("/profile")
-  } catch (error) {
-    next(error)
+      res.redirect("/profile");
+    } catch (error) {
+      next(error);
+    }
   }
+);
 
+// GET "/profile/publications/:publicationId/edit" => renders profile own publication to edit
+router.get(
+  "/publications/:publicationId/edit", isUserLoggedIn, async (req, res, next) => {
+    const { publicationId } = req.params;
+    
+    try {
+      const ownPublicationToEdit = await Publication.findById(publicationId);
+      res.render("profile/publication-edit.hbs", {
+        ownPublicationToEdit: ownPublicationToEdit,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
+// POST "/profile/publications/:publicationId/edit" => renders profile own publication to edit
+router.post(
+  "/publications/:publicationId/edit", isUserLoggedIn, async (req, res, next) => {
+    const { publicationId } = req.params;
+    const {title, content, file} = req.body
+    try {
+      const ownPubEdited = await Publication.findByIdAndUpdate(publicationId, {
+        title,
+        content,
+        file
+      });
+      res.redirect(`/profile/publications/${publicationId}/details`);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-})
-// // POST "/admin/publications/:publicationId/details" => edit current publication and redirects to pending list
-// router.post("/publications/:publicationId/details", async (req, res, next) => {
-// const { publicationId } = req.params;
-// const { title, content, file, approved } = req.body;
-
-// const publicationToUpdate = {
-//   title,
-//   content,
-//   file,
-//   approved,
-// }
-
-// try {
-//   await Publication.findByIdAndUpdate(publicationId, publicationToUpdate, {new: true,});
-
-//   res.redirect("/admin/publications");
-// } catch (error) {
-//   next(error);
-// }
-// });
 
 module.exports = router;
