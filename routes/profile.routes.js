d36fb6fdf738b3e188c52eae0d715588f9dc79e4
David Lazaro <dlazaro7@gmail.com>
2022-10-26 18:56:45 +0200

    my-profile fovourites added

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User.model");
 const Publication = require("../models/Publication.model");
-const uploader = require("../middlewares/cloudinary.js")
+const uploader = require("../middlewares/cloudinary.js");
 
 // Import middlewares
 const {
@@ -10,38 +10,46 @@ const {
   isAdmin,
   isModeratorOrAdmin,
 } = require("../middlewares/auth.middlewares.js");
-const { populate } = require("../models/User.model");
 
 // GET /profile => Renders user profile page
 router.get("/", isUserLoggedIn, async (req, res, next) => {
   try {
-    const userOnlineDetails = await User.findById(req.session.userOnline);
-    const publicationUserOnline = await Publication.find({user: userOnlineDetails,}).populate("user");
+    const userOnlineDetails = await User.findById(req.session.userOnline).populate("favourites");
+    const publicationUserOnline = await Publication.find({
+      user: userOnlineDetails,
+    }).populate("user");
 
-    const clonedPublicationUserOnline = JSON.parse(JSON.stringify(publicationUserOnline))
+    
+
+    const clonedPublicationUserOnline = JSON.parse(
+      JSON.stringify(publicationUserOnline)
+    );
 
     // Publications sorted by created date
-    clonedPublicationUserOnline.sort((a, b) => new Date (b.createdAt) - new Date (a.createdAt))
+    clonedPublicationUserOnline.sort(
+      (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+    );
     // Publications date with timestamps reduce
-    clonedPublicationUserOnline.forEach(eachPublication => {
-      eachPublication.createdAt = new Intl.DateTimeFormat('es-ES', {
+    clonedPublicationUserOnline.forEach((eachPublication) => {
+      eachPublication.createdAt = new Intl.DateTimeFormat("es-ES", {
         timeStyle: "medium",
-        dateStyle: "short"
-      })
-      .format(new Date(eachPublication.createdAt))
+        dateStyle: "short",
+      }).format(new Date(eachPublication.createdAt));
 
-      eachPublication.updatedAt = new Intl.DateTimeFormat('es-ES', {
+      eachPublication.updatedAt = new Intl.DateTimeFormat("es-ES", {
         timeStyle: "medium",
-        dateStyle: "short"
-      })
-      .format(new Date(eachPublication.updatedAt))
+        dateStyle: "short",
+      }).format(new Date(eachPublication.updatedAt));
+    });
 
-    })
+    
 
     res.render("profile/my-profile.hbs", {
       profileDetails: userOnlineDetails,
       publicationUserOnline: clonedPublicationUserOnline,
     });
+
+
   } catch (error) {
     next(error);
   }
@@ -50,11 +58,15 @@ router.get("/", isUserLoggedIn, async (req, res, next) => {
 // *********************** OWN PUBLICATIONS ROUTES *************************************
 // GET "/profile/publications/:publicationId/details" => renders the details of each own publication
 router.get(
-  "/publications/:publicationId/details", isUserLoggedIn, async (req, res, next) => {
+  "/publications/:publicationId/details",
+  isUserLoggedIn,
+  async (req, res, next) => {
     const { publicationId } = req.params;
 
     try {
-      const publicationDetails = await Publication.findById(publicationId).populate("user");
+      const publicationDetails = await Publication.findById(
+        publicationId
+      ).populate("user");
       console.log(publicationDetails);
       res.render("profile/publication-details.hbs", {
         publicationDetails: publicationDetails,
@@ -84,9 +96,11 @@ router.post(
 
 // GET "/profile/publications/:publicationId/edit" => renders profile own publication to edit
 router.get(
-  "/publications/:publicationId/edit", isUserLoggedIn, async (req, res, next) => {
+  "/publications/:publicationId/edit",
+  isUserLoggedIn,
+  async (req, res, next) => {
     const { publicationId } = req.params;
-    
+
     try {
       const ownPublicationToEdit = await Publication.findById(publicationId);
       res.render("profile/publication-edit.hbs", {
@@ -100,14 +114,17 @@ router.get(
 
 // POST "/profile/publications/:publicationId/edit" => renders profile own publication to edit
 router.post(
-  "/publications/:publicationId/edit", isUserLoggedIn, uploader.single("file"), async (req, res, next) => {
+  "/publications/:publicationId/edit",
+  isUserLoggedIn,
+  uploader.single("file"),
+  async (req, res, next) => {
     const { publicationId } = req.params;
-    const {title, content} = req.body
+    const { title, content } = req.body;
     try {
       const ownPubEdited = await Publication.findByIdAndUpdate(publicationId, {
         title,
         content,
-        file: req.file?.path
+        file: req.file?.path,
       });
       res.redirect(`/profile/publications/${publicationId}/details`);
     } catch (error) {
@@ -118,20 +135,25 @@ router.post(
 
 // *********************** FAVOURITE PUBLICATIONS ROUTES *************************************
 // POST "/profile/publications/:publicationId/favourite => add publication to UserOnline.favourite properties
-router.post("/publications/:publicationId/favourite", isUserLoggedIn, async (req, res, next) => {
-  const {publicationId} = req.params
+router.post(
+  "/publications/:publicationId/favourite",
+  isUserLoggedIn,
+  async (req, res, next) => {
+    const { publicationId } = req.params;
 
-  try {
-    
-    const currentPublication = await Publication.findById(publicationId)
-    const userOnline = await User.findById(req.session.userOnline)
-    const currentUser = await User.findByIdAndUpdate(userOnline, {$addToSet: {favourites: currentPublication}}).populate("publications").select("username favourites")
-    
-    
-   
-    console.log(currentUser)
-  } catch (error) {
-    next(error)
+    try {
+      const currentPublication = await Publication.findById(publicationId);
+      const userOnline = await User.findById(req.session.userOnline);
+      const currentUser = await User.findByIdAndUpdate(userOnline, {
+        $addToSet: { favourites: currentPublication },
+      })
+        .populate("publications")
+        .select("username favourites");
+
+      console.log(currentUser);
+    } catch (error) {
+      next(error);
+    }
   }
-})
+);
 module.exports = router;
